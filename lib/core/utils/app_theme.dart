469682026e43d54adfb2774aa0d059f61ba4bb93
MohammedGhazaly <warendragon67@gmail.com>
2023-11-25 04:12:36 +0200

    Change app theme and added text to splash view

--- lib/core/utils/app_theme.dart
+++ lib/core/utils/app_theme.dart
@@ -2,8 +2,12 @@ import 'package:bookly_clean_arch/core/constants/colors.dart';
 import 'package:flutter/material.dart';
 
 class AppTheme {
-  static ThemeData mainTheme = ThemeData(
-    scaffoldBackgroundColor: AppColors.primaryColor,
-    useMaterial3: true,
-  );
+  // static ThemeData mainTheme = ThemeData(
+  //     scaffoldBackgroundColor: AppColors.primaryColor,
+  //     useMaterial3: true,
+  //     brightness: Brightness.dark);
+  static ThemeData mainTheme = ThemeData.dark().copyWith(
+      scaffoldBackgroundColor: AppColors.primaryColor,
+      useMaterial3: true,
+      brightness: Brightness.dark);
 }
