d2dee40885fc7f7f6e186c7f6118a6bdc68fc33a
MohammedGhazaly <warendragon67@gmail.com>
2023-11-26 06:57:52 +0200

    Added sliding and fading animation to splash view

--- lib/features/splash/presentation/view/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/view/widgets/splash_view_body.dart
@@ -1,19 +1,70 @@
 import 'package:bookly_clean_arch/core/constants/assets.dart';
 import 'package:flutter/material.dart';
 
-class SplashViewBody extends StatelessWidget {
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
 
   @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody>
+    with SingleTickerProviderStateMixin {
+  late AnimationController animationController;
+  late Animation<Offset> slidingAnimation;
+  late Animation<double> fadeTransitionAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    animationController =
+        AnimationController(vsync: this, duration: const Duration(seconds: 2));
+    slidingAnimation = Tween<Offset>(begin: Offset(0, 10), end: Offset(0, 0))
+        .animate(animationController);
+    fadeTransitionAnimation =
+        Tween<double>(begin: 0, end: 1).animate(animationController);
+
+    animationController.forward();
+    // slidingAnimation.addListener(() {
+    //   setState(() {});
+    // });
+    // slidingController =
+    //     AnimationController(vsync: this, duration: const Duration(seconds: 1));
+
+    // fadeTransitionAnimation.addListener(() {
+    //   setState(() {});
+    // });
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    animationController.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
+    print("Rebuilt");
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
         Image.asset(AppAssets.logo),
-        const Text(
-          "Read free books",
-          textAlign: TextAlign.center,
+        AnimatedBuilder(
+          animation: slidingAnimation,
+          builder: (context, _) {
+            return FadeTransition(
+              opacity: fadeTransitionAnimation,
+              child: SlideTransition(
+                position: slidingAnimation,
+                child: const Text(
+                  "Read free books",
+                  textAlign: TextAlign.center,
+                ),
+              ),
+            );
+          },
         ),
       ],
     );
