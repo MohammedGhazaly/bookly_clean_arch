d258c93e986712b1f9507698540b8396bb146495
MohammedGhazaly <warendragon67@gmail.com>
2023-11-29 21:25:43 +0200

    Refactor to a generic use case

--- lib/features/home/domain/use_cases/fetch_featured_books_use_case.dart
+++ lib/features/home/domain/use_cases/fetch_featured_books_use_case.dart
@@ -1,13 +1,18 @@
 import 'package:bookly_clean_arch/core/errors/failure.dart';
+import 'package:bookly_clean_arch/core/use_case/use_case.dart';
 import 'package:bookly_clean_arch/features/home/domain/entities/book_entity.dart';
 import 'package:bookly_clean_arch/features/home/domain/repos/home_repo.dart';
 import 'package:dartz/dartz.dart';
 
-class FeaturedBooksUseCase {
+class FeaturedBooksUseCase implements UseCase<List<BookEntity>, NoParam> {
   final HomeRepo homeRepo;
 
   FeaturedBooksUseCase({required this.homeRepo});
-  Future<Either<Failure, List<BookEntity>>> invoke() {
-    return homeRepo.fetchFeaturedBooks();
+
+  // invoke - call - execute
+
+  @override
+  Future<Either<Failure, List<BookEntity>>> invoke([NoParam? param]) async {
+    return await homeRepo.fetchFeaturedBooks();
   }
 }
