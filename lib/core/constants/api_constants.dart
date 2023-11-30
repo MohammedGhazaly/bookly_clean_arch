bf4ffd9965a2cc2743f5f2af5396d8175e3ca89f
MohammedGhazaly <warendragon67@gmail.com>
2023-11-30 06:48:31 +0200

    Create api service

---
+++ lib/core/constants/api_constants.dart
@@ -0,0 +1,3 @@
+class ApiConstants {
+  static String baseUrl = "https://www.googleapis.com/books/v1/volumes/";
+}
---
+++ lib/core/services/api_service.dart
@@ -0,0 +1,12 @@
+import 'package:bookly_clean_arch/core/constants/api_constants.dart';
+import 'package:dio/dio.dart';
+
+class ApiService {
+  final Dio dio;
+
+  ApiService({required this.dio});
+
+  get({required String endPoint}) async {
+    var response = dio.get("${ApiConstants.baseUrl}$endPoint");
+  }
+}
